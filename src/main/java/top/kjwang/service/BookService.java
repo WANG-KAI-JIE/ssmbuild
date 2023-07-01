62debbd2378e132f757f156ea7e2064974ca4545
wkj <1729298243@qq.com>
2023-07-01 13:53:50 +0800

    使用@Param注解给参数命名

--- src/main/java/top/kjwang/service/BookService.java
+++ src/main/java/top/kjwang/service/BookService.java
@@ -15,13 +15,13 @@ public interface BookService {
 	//增加一个Book
 	int addBook(Books book);
 	//根据id删除一个Book
-	int deleteBookById(int id);
+	int deleteBookById(@Param("bookID") int id);
 	//更新Book
 	int updateBook(Books books);
 	//根据id查询,返回一个Book
-	Books queryBookById(int id);
+	Books queryBookById(@Param("bookID") int id);
 	//查询全部Book,返回list集合
 	List<Books> queryAllBook();
 	//	根据书名查询书
-	Books queryBookByName(@Param("bookName" )String bookName);
+	Books queryBookByName(@Param("bookName")String bookName);
 }
